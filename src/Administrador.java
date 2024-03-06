0a38f1691d3171ef514eac1f910a591ea015ac04
Braulioarilop <159542523+Braulioarilop@users.noreply.github.com>
2024-03-06 12:10:49 +0100

    Dia2
    Avance de codigo

--- src/Administrador.java
+++ src/Administrador.java
@@ -1,2 +1,63 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Collection;
 public class Administrador {
+    public static final String contraseña = "1234A";
+    public static ArrayList departamentos = new ArrayList();
+
+    public static void menuadmin(){
+        Scanner in = new Scanner(System.in);
+        boolean logout=false;
+        String opcionAdmin;
+        do{
+            System.out.println("Acceso: Administrador");
+            System.out.println("1. \t Añdir Departamento");
+            System.out.println("2. \t Eliminar Departamento");
+            System.out.println("3. \t Crear nueva sala");
+            System.out.println("4. \t Eliminar sala");
+            System.out.println("5. \t Añdir Departamento");
+            System.out.println("6. \t Cerrar sesion");
+            opcionAdmin = in.nextLine();
+            switch(opcionAdmin){
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+                case "4":
+                    break;
+                case "5":
+                    break;
+                case "6":
+                    System.out.println("Cerrando sesion...");
+                    logout=true;
+                    break;
+                default:
+                    System.out.println("Opcion de administrador no correcta \n Introduzca una opcion valida");
+                    break;
+            }
+        }while(!logout);
+        in.close();
+    }
+    public void AnadirDep(String nombre, String codigo, String representante){
+        Departamento d1 = new Departamento(nombre,codigo,representante);
+        departamentos.add(d1);
+    }
+    public void EliminarDep(String nombre){
+        departamentos.remove(nombre);
+    }
+    public void ListarDep(){
+        for (Object o : departamentos) {
+            System.out.println(o);
+        }
+    }
+    public static int buscar(String nombre) {
+        Iterator iterador;
+        if (iterador)
+        return departamentos.indexOf(d1.getNombre());
+    }
 }
+
+
--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,45 @@
 import java.util.Scanner;
 public class Main {
 
-    public static void Menu(){
+    public static void menu(){
         Scanner in = new Scanner(System.in);
 
         //Centinela que nos permitirá salir
         boolean salir = false;
-
+        String opcion;
         do {
             System.out.println("Bienvenido a la aplicación \n ¿Como desea acceder?");
             System.out.println("1. Login de administrador");
-            System.out.println("2. Login de administrador");
-            System.out.println("3. Login de administrador");
+            System.out.println("2. Login de departamento");
+            System.out.println("3. Salir del programa");
+
+            opcion = in.nextLine();
+            switch(opcion){
+                case "1":
+                    String passwd;
+                    System.out.println("Indicame la contraseña de administrador:");
+                    passwd = in.nextLine();
+                    if (passwd.matches(Administrador.contraseña)){
+                        Administrador.menuadmin();
+                    }else{
+                        System.out.println("Contraseña incorrecta");
+                    }
+                    break;
+                case "2":
+                    System.out.println("Has seleccionado la opcion 2");
+                    break;
+                case "3":
+                    System.out.println("Saliendo");
+                    salir=true;
+                    break;
+                default:
+                System.out.println("No has indicado una opcion correcta");
+                    break;
+            }
+
         }while(!salir);
     };
     public static void main(String[] args) {
-        Main.Menu();
+        Main.menu();
     }
 }
