58ae9d4218aad9822abc3764eeb3ac5bc8628214
AdriXT <adri245yt@gmail.com>
2024-03-06 13:51:31 +0100

    Dia 2 (Commit 2)
    Hemos realizado la mayor parte del metodo EliminarDep

--- src/Administrador.java
+++ src/Administrador.java
@@ -14,23 +14,42 @@ public class Administrador {
             System.out.println("Acceso: Administrador");
             System.out.println("1. \t Añdir Departamento");
             System.out.println("2. \t Eliminar Departamento");
-            System.out.println("3. \t Crear nueva sala");
-            System.out.println("4. \t Eliminar sala");
-            System.out.println("5. \t Añdir Departamento");
-            System.out.println("6. \t Cerrar sesion");
+            System.out.println("3. \t Listar Departamentos");
+            System.out.println("4. \t Crear nueva sala");
+            System.out.println("5. \t Eliminar sala");
+            System.out.println("6. \t Añdir Departamento");
+            System.out.println("7. \t Cerrar sesion");
             opcionAdmin = in.nextLine();
             switch(opcionAdmin){
                 case "1":
+                    String nombre;
+                    String codigo;
+                    String represent;
+                    System.out.println("Creacion de departamento");
+                    System.out.println("Nombre de departamento");
+                    nombre= in.nextLine();
+                    System.out.println("Codigo de departamento");
+                    codigo = in.nextLine();
+                    System.out.println("Representante de departamento");
+                    represent = in.nextLine();
+                    anadirDep(nombre,codigo,represent);
                     break;
                 case "2":
+                    String codigopru;
+                    System.out.println("Dime el codigo del departamento");
+                    codigopru = in.nextLine();
+                    eliminarDep(codigopru);
                     break;
                 case "3":
+                    listarDep();
                     break;
                 case "4":
                     break;
                 case "5":
                     break;
                 case "6":
+                    break;
+                case "7":
                     System.out.println("Cerrando sesion...");
                     logout=true;
                     break;
@@ -41,23 +60,31 @@ public class Administrador {
         }while(!logout);
         in.close();
     }
-    public void AnadirDep(String nombre, String codigo, String representante){
+    public static void anadirDep(String nombre, String codigo, String representante){
         Departamento d1 = new Departamento(nombre,codigo,representante);
         departamentos.add(d1);
     }
-    public void EliminarDep(String nombre){
-        departamentos.remove(nombre);
+    public static void eliminarDep(String nombre){
+        departamentos.remove();
+
     }
-    public void ListarDep(){
+    public static void listarDep(){
         for (Object o : departamentos) {
             System.out.println(o);
         }
     }
-    public static int buscar(String nombre) {
-        Iterator iterador;
-        if (iterador)
-        return departamentos.indexOf(d1.getNombre());
+    public static Departamento buscar(String codigo) {
+        Iterator iterador = departamentos.iterator();
+        boolean esIgual=false;
+        do {
+            Departamento d1 =(Departamento) iterador.next();
+            if (d1.getCodigo().equalsIgnoreCase(codigo)){
+                esIgual=true;
+            }
+        }while(iterador.hasNext());
+        return esIgual;
     }
+    public static
 }
 
 
--- src/Departamento.java
+++ src/Departamento.java
@@ -4,32 +4,45 @@ public class Departamento {
     private String codigo;
     private String representante;
 
-    public Departamento(String nombre,String codigo,String representante){
-        this.nombre=nombre;
-        this.codigo=codigo;
-        this.representante=representante;
+    public Departamento(String nombre, String codigo, String representante) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.representante = representante;
     }
-    public void menuDep(){
-        Scanner Scanner=new Scanner(System.in);
-        String  menu;
-        String opcionDep;
 
+    public void menuDep() {
+        Scanner in = new Scanner(System.in);
+        String opcionDep;
+        boolean salirDep = false;
         do {
             System.out.println("1.Acceso:Departameto");
             System.out.println("2.Escribe el nombre del departamento");
             System.out.println("3.Escribe el codigo del departamento");
             System.out.println("4.escribe el nombre del representante");
             System.out.println("5.Cerrar sesion");
+            opcionDep = in.nextLine();
 
+            switch (opcionDep) {
+                case "1":
+                    break;
+                case "2":
+                    break;
+                case "3":
+                    break;
+            }
+        }while (!salirDep) ;
 
-            switch (opcionDep){
-                case"1":
-                    break;
-                case"2":
-                    break;
-                case"3":
-                    break;
-        }while()
     }
 
+    public String getCodigo() {
+        return codigo;
+    }
+
+    @Override
+    public String toString() {
+        String DepartaCom = "Nombre de departamento: "+ nombre +
+                            "\n Codigo de departamento: "+ codigo +
+                            "\n Representante: " + representante;
+        return DepartaCom;
+    }
 }
