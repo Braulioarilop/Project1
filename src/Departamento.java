8e236eed1047a28c6382043d3fccb5ee0d5f1661
AdriXT <adri245yt@gmail.com>
2024-03-08 09:51:46 +0100

    Dia 4 (Segundo commit)
    Solucionado el problema con git.
    Cambiado el menu departamento.

--- src/Departamento.java
+++ src/Departamento.java
@@ -10,32 +10,27 @@ public class Departamento {
         this.representante = representante;
     }
 
-    public void menuDep() {
+    public static void menuDep() {
         Scanner scanner = new Scanner(System.in);
         int opcion;
         boolean logout =false;
         String opcionDep;
         do {
-            System.out.println("\n Menú del Departamento " + nombre);
-            System.out.println("1. Ver información del departamento");
-            System.out.println("2. Cambiar representante");
-            System.out.println("3. Salir");
+            System.out.println("\n Menú del Departamento");
+            System.out.println("1. Añadir reserva");
+            System.out.println("2. Cancelar reserva");
+            System.out.println("3. Listar todas las reservas");
             System.out.print("Seleccione una opción: ");
             opcion = scanner.nextInt();
             switch (opcion) {
                 case 1:
-                    System.out.println("Nombre: " + nombre);
-                    System.out.println("Código: " + codigo);
-                    System.out.println("Representante: " + representante);
+
                     break;
                 case 2:
-                    System.out.print("Ingrese el nuevo representante: ");
-                    scanner.nextLine(); // Limpiar el buffer del scanner
-                    representante = scanner.nextLine();
-                    System.out.println("Representante cambiado con éxito a: " + representante);
+
                     break;
                 case 3:
-                    System.out.println("Saliendo del Menú del Departamento...");
+
                     break;
                 default:
                     System.out.println("Opción no válida. Por favor, seleccione una opción válida.");
@@ -58,5 +53,9 @@ public class Departamento {
                             "\n Codigo de departamento: "+ codigo +
                             "\n Representante: " + representante;
         return DepartaCom;
+    }
+
+    public void listarSalasConReservas(){
+        Administrador.listarSalas();
     }
 }
